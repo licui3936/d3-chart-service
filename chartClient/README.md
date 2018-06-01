439f15bd23c3d4978408dbeb8aca220941807051
licui <li@openfin.co>
2018-06-01 10:36:34 -0400

    add css file and do some cleanup

--- chartClient/README.md
+++ chartClient/README.md
@@ -1,12 +1,11 @@
 # D3 Chart Application
 
-A simple d3 chart application. The purpose of this repository is to allow users to use chart service to get data and use d3 to render a chart. The setup has minimal dependencies and the feature set is intentionally limited. Any further configuration can be achieved by editing the `app.json` file: [Application Config](https://openfin.co/application-config/).
-
+A simple d3 chart application. The purpose of this repository is to allow users to use chart service to get data and use d3 to render a chart.
 
 ### How to use this:
 
 * Clone this repository: `git clone https://github.com/licui3936/d3-chart-service`
-* Install the dependencies: `cd d3-chart-service` & `npm install`
-* Build the files: `npm run build`
+* Install the dependencies: `cd d3-chart-service/chartClient` & `npm install`
+* Build the files using webpack: `npm run build`
 * Start the server and launch the application: `npm start`
 
--- chartClient/src/index.ts
+++ chartClient/src/index.ts
@@ -1,13 +1,20 @@
+//event listeners.
+document.addEventListener("DOMContentLoaded", () => {
+  if (typeof fin != "undefined") {
+    fin.desktop.main(onMain);
+  } else {
+    let ofVersion: any = document.querySelector("#of-version");
+    ofVersion.innerText =
+      "OpenFin is not available - you are probably running in a browser.";
+  }
+});
+
 //Once the DOM has loaded and the OpenFin API is ready
-if (window.hasOwnProperty("fin")) {
-  const app: any = fin.desktop.Application.getCurrent();
-  fin.desktop.System.showDeveloperTools(app.uuid, app.uuid);
+function onMain() {
+  const app = fin.desktop.Application.getCurrent();
+  //fin.desktop.System.showDeveloperTools(app.uuid, app.uuid);
   fin.desktop.System.getVersion(version => {
-    const ofVersion = document.querySelector("#of-version") as any;
+    let ofVersion: any = document.querySelector("#of-version");
     ofVersion.innerText = version;
   });
-}
-else {
-  const ofVersion = document.querySelector("#of-version") as any;
-  ofVersion.innerText = "OpenFin is not available - you are probably running in a browser.";
 }
