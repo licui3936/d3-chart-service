13ec14a60c33d5c8178362402280f065f8a9c6ed
licui <li@openfin.co>
2018-06-04 10:35:47 -0400

    use d3 library to read a tsv file

--- chartService/src/index.ts
+++ chartService/src/index.ts
@@ -1,3 +1,5 @@
+import { ChartService } from './chartService';
+
 //event listeners.
 document.addEventListener("DOMContentLoaded", () => {
     if (typeof fin != "undefined") {
@@ -10,11 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   
   //Once the DOM has loaded and the OpenFin API is ready
-  function onMain() {
+  async function onMain() {
     const app = fin.desktop.Application.getCurrent();
     //fin.desktop.System.showDeveloperTools(app.uuid, app.uuid);
     fin.desktop.System.getVersion(version => {
       let ofVersion: any = document.querySelector("#of-version");
       ofVersion.innerText = version;
     });
+
+    // register service
+    let service = new ChartService();
+    await service.register();
   }
